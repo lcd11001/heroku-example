0fa46acd3678272e6eab01f981afa814e5885e09
DESKTOP-LCD\lcd26 <dan.luongcong@gameloft.com>
2024-08-19 00:07:35 +0700

    + deploy only build folder into Heroku

--- README.md
+++ README.md
@@ -3,6 +3,22 @@
 heroku login
 ```
 
+### Create a new Heroku app (if you haven't already)
+```
+heroku create your-app-name
+```
+
+### Set the buildpack:
+```
+heroku buildpacks:set https://github.com/timanovsky/subdir-heroku-buildpack
+```
+
+### Specify the subdirectory: 
+Create a .buildpacks file in the root of your project and specify the build directory.
+```
+echo "build" > .buildpacks
+```
+
 ### Deploy
 ```
 git push heroku main
---
+++ build/rooms/MyRoom.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MyRoom = void 0;
+const core_1 = require("@colyseus/core");
+const MyRoomState_1 = require("./schema/MyRoomState");
+class MyRoom extends core_1.Room {
+    constructor() {
+        super(...arguments);
+        this.maxClients = 4;
+    }
+    onCreate(options) {
+        this.setState(new MyRoomState_1.MyRoomState());
+        this.onMessage("ping", (client, message) => {
+            //
+            // create "pong" message
+            //
+            const now = new Date();
+            const formattedDate = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}.${String(now.getMilliseconds()).padStart(3, '0')}`;
+            this.broadcast("pong", {
+                client: client.sessionId,
+                timestamp: formattedDate,
+                message
+            });
+        });
+    }
+    onJoin(client, options) {
+        console.log(client.sessionId, "joined!");
+    }
+    onLeave(client, consented) {
+        console.log(client.sessionId, "left!");
+    }
+    onDispose() {
+        console.log("room", this.roomId, "disposing...");
+    }
+}
+exports.MyRoom = MyRoom;
